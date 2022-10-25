ee41f234af67bb2d3dab8813b816a96427ee2370
Lyubomir Cholakov <l.cholakoff@gmail.com>
2022-10-25 07:49:16 +0200

    Rewrite js almost from scratch.
    I was getting hung up on making keyboard safe input work, adding long hairy lines of code.
    Code was losing structure and calculus wasn't working. New structure is cleaner, with a lot more shorter functions.
    First round of calculation with mouse input works.
    
    TODO: continue calculations after first round. Add safe keyboard input.

--- script.js
+++ script.js
@@ -1,139 +1,218 @@
+// Globals //
+
+const display = document.querySelector('.display');
+let _num1 = 0;
+let _num2 = 0;
+let _operator = '';
+let _decimalCount = 0;
+let _num1decimalCount = 0;
+let _num2decimalCount = 0;
+let _operatorCount = 0;
+
+// Operations //
+
 function add(a, b) {
-    return a + b;	
+    return a + b;
     resetOperatorCount();
-  };
-  
+};
+
 function subtract(a, b) {
     return a - b;
     resetOperatorCount();
-  };
-  
+};
+
 function multiply(a, b) {
     return a * b;
     resetOperatorCount();
-  };
-  
+};
+
 function divide(a, b) {
     return a / b;
     resetOperatorCount();
 }
 
 function operate(a, oper, b) {
-    if (oper === '+') return add(a, b);
-    if (oper === '-') return subtract(a, b);
-    if (oper === '*') return multiply(a, b);
-    if (oper === '/') return divide(a, b);
-    return 'Error: invalid operator';
+    switch (oper) {
+
+        case '+': return add(a, b);
+        case '-': return subtract(a, b);
+        case '*': return multiply(a, b);
+        case '/': return divide(a, b);
+        default: return 'Error: invalid operator';
+    }
 }
 
-const display = document.querySelector('.display');
-let num1 = 0;
-let num2 = 0;
-let operator = '';
-let operatorCount = 0;
+// Input //
 
 document.querySelectorAll('button').forEach(item => {
-  item.addEventListener('click', event => {
-    switch(item.textContent) {
-      case 'C': clear(); break;
-      case '←': backspace(); break;
-      case '00': if(display.textContent[0] !== '0') display.textContent += item.textContent; break; 
-      default: if(isFinite(item.textContent)) {
-                  display.textContent[0] === '0' ?
-                    display.textContent = item.textContent :
-                    display.textContent += item.textContent; 
-                } else if (isOperator && operatorCount === 0) {
-                  ++operatorCount;
-                  display.textContent += item.textContent;                  
-                }
-    };
-    resizeFont();
-  });
+    item.addEventListener('click', event => {
+        parseClick(item.textContent);
+    });
 });
 
 window.addEventListener("keydown", function (event) {
-  // if (event.defaultPrevented) {
-  //   return; // Do nothing if the event was already processed
-  // }
-  
-  switch(event.key) {
-    case 'Backspace': backspace(); break;
-    case 'Escape': 
-      document.querySelector('.clear').classList.toggle('active'); 
-      clear(); 
-      break;
-    default:
-      if(isFinite(event.key)) {
-        digitButton = document.querySelector(`.button-${event.key}`);
-        digitButton.click();
-        digitButton.classList.toggle('active');
-      } else if(isOperator(event.key) && operatorCount === 0) {
-          ++operatorCount;
-          display.textContent += event.key;
-      } else break;
-  }
 
-  if(isOperator(event.key) && operatorCount === 0) {
-      ++operatorCount;
-      display.textContent += event.key;
-  }
-
-  resizeFont();
 }, true);
 
 window.addEventListener("keyup", function (event) {
 
-  
-  switch(event.key) {
-    case 'Backspace': backspace(); break;
-    case "Escape": 
-      document.querySelector('.clear').classList.toggle('active'); 
-      clear(); 
-      break;
-    default:
-      if(isFinite(event.key)) {
-        digitButton = document.querySelector(`.button-${event.key}`);
-        digitButton.classList.toggle('active');
-      } else if(isOperator(event.key) && operatorCount === 0) {
-          ++operatorCount;
-          display.textContent += event.key;
-      } return; // Quit when this doesn't handle the key event.
-  }
-
-  if(isFinite(event.key)) {
-    digitButton = document.querySelector(`.button-${event.key}`);
-    digitButton.classList.toggle('active');
-  }
 }, true);
 
-function clear() {
-  // TODO erase memory
-  display.textContent = 0;
+// Parse //
+
+function parseClick(btnTxt) {
+    switch (btnTxt) {
+        case 'C': clear(); break;
+        case '←': backspace(); break;
+        case '.': decimalInput; break;
+        default: if (isFinite(btnTxt)) {
+            digitInput(btnTxt);
+        } else if (isOperator) {
+            operatorInput(btnTxt);
+        }
+    }
+    resizeFont();
 }
 
-function backspace() {
-  if(isOperator) resetOperatorCount();
-  display.textContent.length > 1 ? 
-    display.textContent = display.textContent.slice(0, -1) :
+function parseKey(key) {
+
+}
+
+function digitInput(digit) {
+    display.textContent[0] === '0' ?
+        display.textContent = digit :
+        display.textContent += digit;
+}
+
+function decimalInput(decimal) {
+
+}
+
+function operatorInput(oper) {
+    if (_operatorCount === 0) {
+        display.textContent += oper;
+        _operator = oper;
+        ++_operatorCount;
+    } else {
+        parseDisplay();
+        resetOperatorCount;
+        if (oper !== '=') {
+            display.textContent += oper;
+        }
+    }
+}
+
+function parseDisplay() {
+    let i = 0;
+    for (i; i < display.textContent.length; ++i) {
+        if (isOperator(display.textContent[i])) {
+            break;
+        }
+    }
+
+    _num1 = Number(display.textContent.slice(0, i));
+    _operator = display.textContent[i];
+    _num2 = Number(display.textContent.slice(i + 1));
+
+    console.log(_num1, _operator, _num2);
+    console.log(operate(_num1, _operator, _num2));
+    display.textContent = operate(_num1, _operator, _num2);
+}
+
+// STATE LOGIC
+
+// operator button disable when: one operator has been added. If erase, check whether to undo disable.If
+// decimal disable when: one decimal per current Number.
+// zero/ 00 disable when: operator is divide
+
+// Input logic -> if char is digit -> input, if operator -> save num
+
+// Parse string on every input and decide state
+
+// Button Funcs //
+
+function clear() {
+    _num1 = 0;
+    _num2 = 0;
+    _operator = '';
+    _operatorCount = 0;
+    _num1decimalCount = 0;
+    _num2decimalCount = 0;
     display.textContent = 0;
 }
 
+function backspace(char) {
+    let lastChar = display.textContent[display.textContent.length - 1];
+    if (isOperator(lastChar)) resetOperatorCount();
+    if (isDecimal(lastChar))
+        display.textContent.length > 1 ?
+            display.textContent = display.textContent.slice(0, -1) :
+            display.textContent = 0;
+}
+
+// Bool check Funcs //
+
 function isOperator(a) {
-  return a === '/' || a === '*' || a === '-' || a === '+' || a === '=' || a === 'Enter';
+    return a === '/' || a === '*' || a === '-' || a === '+' || a === '=' || a === 'Enter';
+}
+
+function isDecimal(a) {
+    return a === '.';
+}
+
+// Reset Funcs //
+
+function resetOperatorCount() {
+    _operatorCount = 0;
+}
+
+function resetDecimalCount() {
+    _decimalCount = 0;
+}
+
+function resetValues() {
+
+}
+
+// Helper Funcs //
+
+function num2fromDisplay() {
+    for (let i = 0; i < display.textContent.length; ++i) {
+        if (isOperator(display.textContent[i])) {
+            return display.textContent.slice(i + 1);
+        }
+    }
+    return NaN;
+}
+
+function operator2string(oper) {
+    switch (oper) {
+        case '/':
+            return 'divide';
+            break;
+        case '*':
+            return 'multiply';
+            break;
+        case '-':
+            return 'subtract';
+            break;
+        case '+':
+            return 'add';
+            break;
+        case '=':
+            return 'equal';
+            break;
+    }
 }
 
 // Hardcoded. Some day should be changed to dynamic 
 // comparison between element and text width
 function resizeFont() {
-  // TODO add min font size, parse style.fontSize
-  //&& Number(display.style.fontSize.toString.splice(0, 1)) > 10
-  if(display.textContent.length > 24) {
-    display.style.fontSize = '6px';
-  } else if(display.textContent.length > 12) {
-    display.style.fontSize = '12px';
-  } else display.style.fontSize = '24px';
-}
-
-function resetOperatorCount() {
-  operatorCount === 0;
+    // TODO add min font size, parse style.fontSize
+    //&& Number(display.style.fontSize.toString.splice(0, 1)) > 10
+    if (display.textContent.length > 24) {
+        display.style.fontSize = '6px';
+    } else if (display.textContent.length > 12) {
+        display.style.fontSize = '12px';
+    } else display.style.fontSize = '24px';
 }
