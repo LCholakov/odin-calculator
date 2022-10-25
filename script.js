d389377ae9ce5549c9488fb66e4a2a9b862b1091
Lyubomir Cholakov <l.cholakoff@gmail.com>
2022-10-25 13:01:37 +0200

    Add keyboard support. Finilize assignment.
    Add keyboard support.
    Add rounding to 5 decimals.
    Clean up debug logs.

--- script.js
+++ script.js
@@ -14,31 +14,26 @@ let _hasError = false;
 // Operations //
 
 function add(a, b) {
-  console.log('add');
   return a + b;
   resetOperatorCount();
 };
 
 function subtract(a, b) {
-  console.log('subtract');
   return a - b;
   resetOperatorCount();
 };
 
 function multiply(a, b) {
-  console.log('multiply');
   return a * b;
   resetOperatorCount();
 };
 
 function divide(a, b) {
-  console.log('divide');
   return a / b;
   resetOperatorCount();
 }
 
 function operate(a, oper, b) {
-  console.log('operate');
   switch (oper) {
 
     case '+': return add(a, b);
@@ -61,17 +56,16 @@ document.querySelectorAll('button').forEach(item => {
 });
 
 window.addEventListener("keydown", function (event) {
-
+  parseKeyDown(event.key);
 }, true);
 
 window.addEventListener("keyup", function (event) {
-
+  parseKeyUp(event.key);
 }, true);
 
 // Parse //
 
 function parseClick(btnTxt) {
-  console.log('Parse Click');
   if (_hasError) clear();
   switch (btnTxt) {
     case 'C': clear(); break;
@@ -87,12 +81,109 @@ function parseClick(btnTxt) {
   resizeFont();
 }
 
-function parseKey(key) {
-
+function parseKeyDown(key) {
+  switch (key) {
+    case 'Escape':
+      document.querySelector('.clear').classList.add('active');
+      clear();
+      break;
+    case 'Backspace':
+      document.querySelector('.backspace').classList.add('active');
+      backspace();
+      break;
+    case '.':
+      document.querySelector('.decimal').classList.add('active');
+      decimalInput();
+      break;
+    case '=':
+      document.querySelector('.equal').classList.add('active');
+      equalSignInput();
+      break;
+    case 'Enter':
+      document.querySelector('.equal').classList.add('active');
+      equalSignInput();
+      break;
+    default:
+      if (isFinite(key)) {
+        document.querySelector(`.button-${key}`).classList.add('active');
+        digitInput(key);
+        break;
+      } else if (isOperator(key)) {
+        document.querySelector(`.${operator2string(key)}`).classList.add('active');
+        operatorInput(key);
+        break;
+      } else break;
+  }
+  resizeFont();
 }
 
+function parseKeyUp(key) {
+  switch (key) {
+    case "Escape":
+      document.querySelector('.clear').classList.remove('active');
+      clear();
+      break;
+    case 'Backspace':
+      document.querySelector('.backspace').classList.remove('active');
+      backspace();
+      break;
+    case '.':
+      document.querySelector('.decimal').classList.remove('active');
+      break;
+    case '=':
+      document.querySelector('.equal').classList.remove('active');
+      break;
+    case 'Enter':
+      document.querySelector('.equal').classList.remove('active');
+      break;
+    default:
+      if (isFinite(key)) {
+        document.querySelector(`.button-${key}`).classList.remove('active');
+        break;
+      } else if (isOperator(key)) {
+        document.querySelector(`.${operator2string(key)}`).classList.remove('active');
+        break;
+      } else break;
+  }
+}
+
+function parseDisplay() {
+  // Find Operator
+  let i = 0;
+  for (i; i < display.textContent.length; ++i) {
+    if (isOperator(display.textContent[i])) {
+      if (i >= 1 && isFinite(display.textContent[i - 1])) { // ignore negative numbers
+        break;
+      }
+    }
+  }
+  
+  // return if no operator is found
+  if (i === display.textContent.length) {
+    return;
+  }
+
+  _num1 = Number(display.textContent.slice(0, i));
+  _operator = display.textContent[i];
+  _num2 = Number(display.textContent.slice(i + 1));
+
+  if (_operator === '/' && _num2 === 0) {
+    display.textContent = '🚀 To Infinity! And Beyond! 🚀';
+    _hasError = true;
+  } else {
+    // Calculate and round to 5 decimal places. + in front to drop ending zeroes
+    let result = +operate(_num1, _operator, _num2).toFixed(5);
+    if (!Number.isInteger(result)) {
+      _decimalCount = 1;
+    }
+    display.textContent = result;
+    _isFirstRound = false;
+  }
+}
+
+// Input
+
 function digitInput(digit) {
-  console.log('Digit Input');
   display.textContent[0] === '0' && _isFirstRound ?
     display.textContent = digit :
     display.textContent += digit;
@@ -106,15 +197,12 @@ function decimalInput() {
 }
 
 function operatorInput(oper) {
-  console.log('Operator Input');
   let lastChar = display.textContent[display.textContent.length - 1]
-  console.log(lastChar);
 
   // if it is first operator, add to string
   if (_operatorCount === 0) {
     // if previous symbol is an operator, replace it
     if (isOperator(lastChar)) {
-      console.log('lastChar is operator');
       display.textContent = display.textContent.slice(0, -1) + oper;
       _operator = oper;
       ++_operatorCount;
@@ -130,7 +218,6 @@ function operatorInput(oper) {
     parseDisplay();
     resetOperatorCount;
     display.textContent += oper;
-    console.log(oper, display.textContent);
   }
 }
 
@@ -143,42 +230,6 @@ function equalSignInput() {
   resetOperatorCount();
 }
 
-function parseDisplay() {
-  console.log('Parse Display');
-  let i = 0;
-  for (i; i < display.textContent.length; ++i) {
-    if (isOperator(display.textContent[i])) {
-      if (i >= 1 && isFinite(display.textContent[i - 1])) { // ignore negative numbers
-        break;
-      }
-    }
-  }
-
-  _num1 = Number(display.textContent.slice(0, i));
-  _operator = display.textContent[i];
-  _num2 = Number(display.textContent.slice(i + 1));
-
-  console.log(_num1, _operator, _num2);
-  console.log(operate(_num1, _operator, _num2));
-  if (_operator === '/' && _num2 === 0) {
-    display.textContent = '🚀 To Infinity! And Beyond! 🚀';
-    _hasError = true;
-  } else {
-    display.textContent = operate(_num1, _operator, _num2);
-    _isFirstRound = false;
-  }
-}
-
-// STATE LOGIC
-
-// operator button disable when: one operator has been added. If erase, check whether to undo disable.If
-// decimal disable when: one decimal per current Number.
-// zero/ 00 disable when: operator is divide
-
-// Input logic -> if char is digit -> input, if operator -> save num
-
-// Parse string on every input and decide state
-
 // Button Funcs //
 
 function clear() {
@@ -186,8 +237,7 @@ function clear() {
   _num2 = 0;
   _operator = '';
   _operatorCount = 0;
-  _num1decimalCount = 0;
-  _num2decimalCount = 0;
+  _decimalCount = 0;
   _isFirstRound = true;
   _hasError = false;
   display.textContent = 0;
@@ -200,7 +250,7 @@ function backspace() {
   display.textContent.length > 1 ?
     display.textContent = display.textContent.slice(0, -1) :
     display.textContent = 0;
-
+  resizeFont();
 }
 
 // Bool check Funcs //
@@ -209,9 +259,9 @@ function isOperator(a) {
   return a === '/' || a === '*' || a === '-' || a === '+';
 }
 
-function isEqualSign(a) {
-  return a === '=' || a === 'Enter';
-}
+// function isEqualSign(a) {
+//   return a === '=' || a === 'Enter';
+// }
 
 function isDecimal(a) {
   return a === '.';
@@ -256,9 +306,6 @@ function operator2string(oper) {
     case '+':
       return 'add';
       break;
-    case '=':
-      return 'equal';
-      break;
   }
 }
 
@@ -272,9 +319,9 @@ function resizeFont() {
 
   // TODO add min font size, parse style.fontSize
   //&& Number(display.style.fontSize.toString.splice(0, 1)) > 10
-  if (display.textContent.length > 24) {
+  if (display.textContent.length > 22) {
     display.style.fontSize = '6px';
-  } else if (display.textContent.length > 12) {
+  } else if (display.textContent.length > 11) {
     display.style.fontSize = '12px';
   } else display.style.fontSize = '24px';
 }
